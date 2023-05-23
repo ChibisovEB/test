3b2e1edea22efed4a3b386c7e3122ec7f60bc0bf
Landgraf <e4ibisoff@ya.ru>
2023-05-23 23:07:29 +0700

    final commit

--- test.py
+++ test.py
@@ -19,21 +19,36 @@
 #     res = -1
 # print(res)
 
-# task 13
-MidTempData = []
-N = int(input("Введите общее количество дней (от 1 до 100): "))
-i = 0
-while i < N:
-    MidTempData.append(int(input(f"{i} {N} Введите среднесуточную температуру (от -50 до +50) за {i+1} число: ")))
-    i += 1
-print(MidTempData)
+# # task 13
+# MidTempData = []
+# N = int(input("Введите общее количество дней (от 1 до 100): "))
+# i = 0
+# while i < N:
+#     MidTempData.append(int(input(f"{i} {N} Введите среднесуточную температуру (от -50 до +50) за {i+1} число: ")))
+#     i += 1
+# print(MidTempData)
 
-LongPeriod = 0
-temp = 0
-for i in MidTempData:
-    if i > 0:
-        temp += 1
-    else:
-        if temp > LongPeriod: LongPeriod = temp
-        temp = 0
-print(LongPeriod)
+# LongPeriod = 0
+# temp = 0
+# for i in MidTempData:
+#     if i > 0:
+#         temp += 1
+#     elif temp > LongPeriod: 
+#         LongPeriod = temp
+#         temp = 0
+# if temp > LongPeriod: LongPeriod = temp
+# print(LongPeriod)
+
+
+# task 15
+n = int(input("Введите количество арбузов: "))
+massaMin = 1000
+massaMax = 0
+print(massaMin, massaMax)
+for arbuz in range(1,n + 1):
+    massa = int(input(f"Введите массу {arbuz} арбуза: "))
+    if massa < massaMin: massaMin = massa
+    if massa > massaMax: massaMax = massa
+    
+
+print(massaMin, massaMax)
